88370ff6960cab0c32a03bcd9ce96a02ccbec4d1
Vinicius Bernal <vinicius.bernal@present-technologies.com>
2023-05-31 12:44:03 +0100

    Fix home page layout

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -32,67 +32,85 @@ const Home = () => {
   };
 
   return (
-    <div
-      style={{
-        width: '100%',
-        maxWidth: '500px',
-        padding: '50px',
-      }}
-    >
-      <h1>Register</h1>
+    <div className="container">
+      <div className="content">
+        <div className="content-title">
+          <h1>Register</h1>
+        </div>
 
-      <Button
-        click={() => {}}
-        variant="positive"
-        label="Log In With Facebook"
-        icon={FacebookSVG}
-      />
-      <Button
-        click={() => {}}
-        variant="primary"
-        label="Log In With Google"
-        icon={GoogleSVG}
-      />
-      <h2>Create an Account</h2>
-      <TextInput
-        value={data.fullName}
-        change={(event) => handleInputChange(event, 'fullName')}
-        placeholder="Full Name"
-        type="text"
-        variant="neutral"
-      />
-      <TextInput
-        value={data.email}
-        change={(event) => handleInputChange(event, 'email')}
-        placeholder="Enter your email"
-        type="email"
-        variant="neutral"
-      />
-      <TextInput
-        value={data.password}
-        change={(event) => handleInputChange(event, 'password')}
-        placeholder="Password"
-        type="password"
-        variant="neutral"
-      />
+        <div className="form">
+          <div>
+            <div className="social-buttons">
+              <Button
+                click={() => {}}
+                variant="positive"
+                label="Log In With Facebook"
+                icon={FacebookSVG}
+              />
+            </div>
+            <div className="social-buttons">
+              <Button
+                click={() => {}}
+                variant="primary"
+                label="Log In With Google"
+                icon={GoogleSVG}
+              />
+            </div>
+          </div>
+          <h2>Create an Account</h2>
+          <div>
+            <div className="social-buttons">
+              <TextInput
+                value={data.fullName}
+                change={(event) => handleInputChange(event, 'fullName')}
+                placeholder="Full Name"
+                type="text"
+                variant="neutral"
+              />
+            </div>
+            <div className="social-buttons">
+              <TextInput
+                value={data.email}
+                change={(event) => handleInputChange(event, 'email')}
+                placeholder="Enter your email"
+                type="email"
+                variant="neutral"
+              />
+            </div>
+            <div className="social-buttons">
+              <TextInput
+                value={data.password}
+                change={(event) => handleInputChange(event, 'password')}
+                placeholder="Password"
+                type="password"
+                variant="neutral"
+              />
+            </div>
+          </div>
 
-      <Button
-        click={handleSignUp}
-        variant="info"
-        label="Sign Up"
-        disabled={!formValid}
-        data-testid="sign-up-button"
-      />
+          <div className="social-buttons">
+            <Button
+              click={handleSignUp}
+              variant="info"
+              label="Sign Up"
+              disabled={!formValid}
+              data-testid="sign-up-button"
+            />
+          </div>
+          <div className="form-footer">
+            <div>
+              <span className="account">
+                Need an account? <span className="account-login">Log in</span>
+              </span>
+            </div>
+            <div className="social-buttons">
+              <span className="account-password">Forget Password</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
-
-/**
- * os comopnentes estão criado ja com os testes, agora falta fazer:
- *
- * - Layout da tela home
- * - Testes tela home
- * - responsividade
- */
